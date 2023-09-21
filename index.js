8b02b5dade70125b57de5b1410e5b255a5da1695
drewschmidt145 <drewschmidt145@gmail.com>
2023-09-21 14:24:32 -0700

    Added endpoints
    when active, table of endpoints should be added to documentation.html and start of endpoints in index.js

--- index.js
+++ index.js
@@ -6,58 +6,49 @@ const app = express();
 // for logging in terminal
 app.use(morgan('common'));
 
-// get request
+// start endpoint
 app.get('/', (req, res) => {
     res.send('Welcome to myFlix!');
 });
 
-let topMovies = [
-    {
-        title: 'The Dark Knight',
-        director: 'Christopher Nolan'
-    },
-    {
-        title: 'Pulp Fiction',
-        director: 'Quentin Tarantino'
-    },
-    {
-        title: 'Fight Club',
-        director: 'David Fincher'
-    },
-    {
-        title: 'Interstellar',
-        director: 'Christopher Nolan'
-    },
-    {
-        title: 'Back to the Future',
-        director: 'Robert Zemeckis'
-    },
-    {
-        title: 'Scarface',
-        director: 'Brian De Palma'
-    },
-    {
-        title: 'Up',
-        director: 'Pete Docter'
-    },
-    {
-        title: 'Kill Bill: Vol. 1',
-        director: 'Quentin Tarantino'
-    },
-    {
-        title: 'Star Wars: Episode VI - Return of the Jedi',
-        director: 'Richard Marquand'
-    },
-    {
-        title: 'Django Unchained',
-        director: 'Quentin Tarantino'
-    },
-];
+// endpoints
 
+// get a list of all movies
 app.get('/movies', (req, res) => {
-    res.json(topMovies);
+    res.send('Successful GET request for list of movies');
 });
-
+// get information on 1 movie
+app.get('/movies/:Title', (req, res) => {
+    res.send('Successful GET request for movie');
+});
+// get information and sort by genre
+app.get('/genres/:Name', (req, res) => {
+    res.send('Successful GET request for genre');
+});
+// get information regarding specific director
+app.get('/directors/:Name', (req, res) => {
+    res.send('Successful GET request for director');
+});
+// create user using information provided
+app.post('/users', (req, res) => {
+    res.send('Successfully created user.');
+});
+// updated user information with new data provided
+app.put('/users/:id', (req, res) => {
+    res.send('Successfully updated users account.');
+});
+// add a movie to users favorite movie list
+app.post('/users/:Username/movies/:MovieID', (req, res) => {
+    res.send('Successfully added movie from favorites list.')
+});
+// delete a movie from users favorite movie list
+app.delete('/users/:Username/movies/:MovieID', (req, res) => {
+    res.send('Successfully deleted movie from favorites list.')
+});
+// deletes users accounts
+app.delete('/users/:id', (req, res) => {
+    res.send('Successfully deleted users account.');
+});
 
 // for getting files in public file
 app.use(express.static('public'));
