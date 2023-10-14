e6578f772a19972e74b6d0039c893d338a415c7a
drewschmidt145 <drewschmidt145@gmail.com>
2023-10-14 11:43:58 -0700

    Update index.js

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ const Users = models.User;
 
 dotenv.config({path:__dirname+'/.env'});
 
-mongoose.connect('mongodb://localhost:27017/myflix', { useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose.connect('mongodb://localhost:27017/myflix', { useNewUrlParser: true, useUnifiedTopology: true });
 
-// mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
