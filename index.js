e4d88d43f2b4477e7e758d309cdd5fb86814eec9
drewschmidt145 <drewschmidt145@gmail.com>
2023-10-27 13:03:59 -0700

    Delete authenication for /movies endpoint

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.get('/', (req, res) => {
 });
 
 // get a list of all movies
-app.get('/movies', passport.authenticate('jwt', {session: false}), (req, res) => {
+app.get('/movies', (req, res) => {
     Movies.find()
     .then((movies) => {
         res.status(201).json(movies);
