faf6c8ab55576828ef76eaf31200f8ec2159358c
drewschmidt145 <drewschmidt145@gmail.com>
2023-12-05 13:39:58 -0700

    add CORS

--- index.js
+++ index.js
@@ -21,7 +21,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const cors = require('cors');
-app.use(cors());
+let allowedOrigins = ["https://main--myflix-1982.netlify.app"];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  }
+}));
+
 
 // for logging in terminal
 app.use(morgan('common'));
