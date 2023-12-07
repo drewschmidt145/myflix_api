c3ed881e7286115659b27b9c4784c865282cf660
drewschmidt145 <drewschmidt145@gmail.com>
2023-12-07 16:06:10 -0700

    Changed allowedOrigins that have access, removed forward slash

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const cors = require('cors');
-let allowedOrigins = ["https://main--myflix-2891.netlify.app/", "http://localhost:1234/"];
+let allowedOrigins = ["https://main--myflix-2891.netlify.app", "http://localhost:1234"];
 
 app.use(cors({
   origin: (origin, callback) => {
